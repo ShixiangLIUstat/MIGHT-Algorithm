3e135f9779e2467c095334a8b6508c750c12b798
ShixiangLIUstat <82197949+ShixiangLIUstat@users.noreply.github.com>
2026-05-24 09:06:59 +0800

    Update Additional simulation.R

--- Additional simulation.R
+++ Additional simulation.R
@@ -37,7 +37,7 @@ source('./simuL.R')
 # lam=1; rate0 = 0.5; stren=0; scale=22; type="ER";
 # t1= Sys.time()
 # myhat = simu_L(seed=123, n=100, K=10, p=100,
-#        link=0.05, gamma=1, lam=1, rate0= 0.5, stren=0, scale=22 )
+#        link=0.05, gamma=1, lam=1, rate0= 0.5, stren=0, scale=17 )
 # t2=Sys.time()
 # t2-t1
 
@@ -88,6 +88,7 @@ source('./simuL.R')
    
   PAtemp = simu_L(seed=mc, n=100, K=10, p=200, link=0.05, gamma=1, type="scale",
                           distr = "norm", lam=1, stren=0, scale = 17)
+   
   TRY10010[[mc]] = list( ER   = ERtemp,
                         band = Bandtemp,
                         hub  = Hubtemp,
@@ -95,5 +96,62 @@ source('./simuL.R')
   
   save(TRY10010, file = "rev_n100K10.RData")
   }
+
+
+
+ ##### 3. n=200; K=10 #####
+  MC = 50; 
+  n=200; K=10; p=200;
   
- 
+  TRY20010 = list() 
+  
+  for(mc in 1:MC){
+  ERtemp = simu_L(seed=mc, n=200, K=10, p=200, link=0.05, gamma=1, type="ER",
+                  lam=1, rate0=0.5, stren=0, scale = 17)
+  
+  Bandtemp = simu_L(seed=mc, n=200, K=10, p=200, link=0.05, gamma=1, type="band",
+                    lam=1, rate0=0.5, stren=0, scale = 17)
+  
+  Hubtemp = simu_L(seed=mc, n=200, K=10, p=200, link=0.05, gamma=1, type="hub",
+                   lam=1, rate0=0.5, stren=0, scale = 17)
+   
+  PAtemp = simu_L(seed=mc, n=100, K=10, p=200, link=0.05, gamma=1, type="scale",
+                          distr = "norm", lam=1, stren=0, scale = 17)
+   
+  TRY20010[[mc]] = list( ER   = ERtemp,
+                        band = Bandtemp,
+                        hub  = Hubtemp,
+                        scalefree = PAtemp ) 
+  
+  save(TRY20010, file = "rev_n200K10.RData")
+  }
+
+
+ ##### 4. heavy tail t distribution #####
+MC = 50
+scale = 15
+Heavy = list()
+
+for( mc in 1:MC ){
+  temp = list()
+  
+  t1= Sys.time()
+  temp[[1]] = simu_L(seed=10*mc+1, n=100, K=10, p=100, type="ER", distr=3,
+                     link=0.1, gamma=1, lam=1, rate0= 0.5, stren=0, scale= scale ) 
+  temp[[1]]; t2=Sys.time() 
+  
+  t1= Sys.time()
+  temp[[2]] = simu_L(seed=10*mc+2, n=100, K=10, p=100, type="ER", distr=6,
+                     link=0.1, gamma=1, lam=1, rate0= 0.5, stren=0, scale= scale )
+  temp[[2]]; t2=Sys.time() 
+  
+  t1= Sys.time()
+  temp[[3]] = simu_L(seed=10*mc+3, n=100, K=10, p=100, type="ER", distr=9,
+                     link=0.1, gamma=1, lam=1, rate0= 0.5, stren=0, scale= scale)
+  temp[[3]]; t2=Sys.time() 
+
+  Heavy[[mc]] = temp 
+  save(Heavy, file = "rev_robust.RData")
+}
+
+
