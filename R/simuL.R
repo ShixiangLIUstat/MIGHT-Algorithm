32c73adb1923c34922b4033de401916dcff0783f
ShixiangLIUstat <82197949+ShixiangLIUstat@users.noreply.github.com>
2026-05-24 08:54:46 +0800

    Update simuL.R

--- R/simuL.R
+++ R/simuL.R
@@ -19,28 +19,55 @@ source('./JGL MIGHT.R')
 source('./JGL.R')
 source('./separate.R')
 
-# link    (0< <1) The probability of the main ER graph
+# link    (0< <1) The probability of the main ER graph (only used in ER graph)
 # gamma   (>=0) The tunning parameter in EBIC
+# type    (default "ER") structure of graphs: ER, band, hub, and scale
+# distr   (default "norm") distribution 
 # lam     (DO NOT CHANGE) JUST TAKE IT AS 1
-# rate0   rate of support each graph randomly drop from the main graph
-# stren   the signal strength of variance, the bigger, the singal strength weaker
-
-simu_L <- function(seed, n, K, p, link=0.1, gamma=1,
+# rate0   rate of support each graph randomly drops from the main graph (used in ER and band graph)
+# stren   the signal strength of variance, the bigger, the signal strength weaker
+# scale   the scaling parameter used in MIGHT
+#
+#
+simu_L <- function(seed, n, K, p, link=0.05, gamma=1,
+                    type="ER", distr = "norm",
                      lam=1, rate0=0.5, stren=0, scale =13) {
   
   set.seed(seed)
 
   #generate support location
-  data <- generate_Gamma_L(p, link=link )
   
-  #generate the precsion matrix for each task 
-  Precision <- lapply(1:K, function(x) 
-    generate_Omega_L(data$Gamma, lam=lam, rate0 = rate0, stren=stren) )
+  if(type %in% c("ER", "band") ){
+    data <- generate_Gamma_L(p, link=link, type=type )
+    
+    #generate the precision matrix for each task 
+    Precision <- lapply(1:K, function(x) 
+      generate_Omega_L(data$Gamma, lam=lam, rate0 = rate0, stren=stren) )
+  }else if(type=="hub"){
+    
+    # generate with randomly changed hub structure 
+    Precision = generate_Omega_L_Hub(p=p, K=K, A=3, bn=40, stren=stren, seed=seed)
+  }else if(type=="scale"){
+    
+    # generate with randomly changed PA structure 
+    Precision = generate_Omega_Scale_free(p=p, K=K, A=3, bn=4, stren=stren, seed=seed) 
+  }
   
   #get Covariance of each task
   Sigma <- lapply(Precision, function(x) solve(x))
   
-  X <- lapply(Sigma, function(x) rmvn(n, rep(0, p), x))
+  if(distr == "norm"){
+    X <- lapply(Sigma, function(x) rmvn(n, rep(0, p), x))
+  }else{ # generate t distribution
+    X <- lapply(Sigma, function(x){
+                    # t distribution with degree distr and variance 1
+                    temp = matrix( rt(n*p,df=distr)/sqrt( distr/(distr-2) ), p, n ) 
+                    ev = eigen(x)
+                    Sighalf = ev$vectors %*% diag(sqrt(ev$values))  
+                    res = Sighalf %*% temp
+                    return( t(res) )
+    } )
+  }
   
   res = matrix(0,6, 8)
   colnames(res) = c("Frobenius", "max_L2",
